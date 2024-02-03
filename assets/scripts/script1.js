e6e418d02a1fabd80e58575135fe688e4b98cc78
mvalladares-2019607 <mvalladares-2019607@kinal.edu.gt>
2024-02-03 15:59:04 -0600

    se creó la primer función del script

--- assets/scripts/script1.js
+++ assets/scripts/script1.js
@@ -1,1 +1,16 @@
-
+let tasks = [];
+ 
+function addTask() {
+  const taskInput = document.getElementById('task');
+  const priorityInput = document.getElementById('priority');
+ 
+  const task = taskInput.value.trim();
+  const priority = priorityInput.value;
+ 
+  if (task !== "") {
+    tasks.push({ task, priority });
+    renderTasks();
+    taskInput.value = "";
+    priorityInput.value = "importante";
+  }
+}
